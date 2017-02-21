7d6bd4b7cf006d42deca494d1abdddb3d874f48e
damienr74 <damienr74@gmail.com>
2017-02-20 22:41:44 -0400

    refactor image upload add backend interface
    Authors: Damien Robichaud & Francis Bosse

---
+++ database/src/main/java/com/piccritic/database/post/Post.java
@@ -0,0 +1,8 @@
+package com.piccritic.database.post;
+
+import java.io.Serializable;
+
+public class Post implements Serializable, Cloneable {
+
+	private static final long serialVersionUID = 8483096248246647524L;
+}
---
+++ website/src/main/java/com/piccritic/website/PicCritic.java
@@ -0,0 +1,59 @@
+package com.piccritic.website;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.piccritic.website.post.CreatePost;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+import compute.piccritic.compute.post.PostServiceInterface;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of a html page where a Vaadin application is
+ * embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class PicCritic extends UI {
+
+	private static final long serialVersionUID = 1L;
+	public PostServiceInterface postService;
+
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
+
+		Button button = new Button("Get Project Name");
+		button.addClickListener(e -> {
+			Window createPost = new CreatePost(getHandle());
+			addWindow(createPost);
+		});
+
+		layout.addComponent(button);
+		layout.setMargin(true);
+		layout.setSpacing(true);
+
+		setContent(layout);
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = PicCritic.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+
+		private static final long serialVersionUID = 1L;
+	}
+
+	private String getHandle() {
+		return "userHandle";
+	}
+}
