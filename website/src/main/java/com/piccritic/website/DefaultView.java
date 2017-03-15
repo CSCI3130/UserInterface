0f4b357a92cd09668af353725293cff4c606ae38
Amirreza Abbasnejad <rezdamir@gmail.com>
2017-03-15 20:51:11 -0300

    Implement Home View with click to post view

--- website/src/main/java/com/piccritic/website/DefaultView.java
+++ website/src/main/java/com/piccritic/website/DefaultView.java
@@ -1,10 +1,12 @@
 package com.piccritic.website;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.piccritic.database.post.Post;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Panel;
 
 /**
  * This class implements the home page for the PicCritic App
@@ -12,14 +14,23 @@ import com.vaadin.ui.Panel;
  * @author Damien Robichaud <br> Francis Bosse
  *
  */
-public class DefaultView extends Panel implements View {
+public class DefaultView extends PostQuickView implements View {
+	List<Post> posts = new ArrayList<>();
+	
+	 public DefaultView(){
+		 for (int i = 0; i < 9; i++) {
+			 posts.add(PicCritic.postService.getPost("users/rezdamir/CUH5F1Z0R8ZvxZG8NacF-kjODTGRWsqW"));
+		 }
+		 func(posts);
+	 }
+	
 	
 	public static final String NAME = "home";
+	
 
 	@Override
 	public void enter(ViewChangeEvent event) {
-		setSizeFull();
-		setContent(new Label("Welcome to Pic Critic!!"));
+		//addComponent(new Label("Welcome to Pic Critic!!"));
 	}
 
 }
